62166b9b244343f9974757ff4cdb665f552f1e55
Natasha <natasha.flausino@hotmail.com>
2021-02-20 17:49:47 -0300

    mod.

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: nbarreir <nbarreir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 03:09:37 by nbarreir          #+#    #+#             */
-/*   Updated: 2021/02/20 03:46:03 by nbarreir         ###   ########.fr       */
+/*   Updated: 2021/02/20 17:38:01 by nbarreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	}
 	if (i < n && (s1[i] == '\0' || s2[i] == '\0'))
 	{
-		return (s1[i] - s2[i]);
+		return ((unsigned char)(s1[i]) - (unsigned char)(s2[i]));
 	}
 	return (0);
 }
