ff77bc3730996f043584c2744c0ff9321056e087
Natasha <natasha.flausino@hotmail.com>
2021-02-22 15:02:18 -0300

    .

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nbarreir <nbarreir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/19 00:01:32 by nbarreir          #+#    #+#             */
-/*   Updated: 2021/02/22 04:23:39 by nbarreir         ###   ########.fr       */
+/*   Updated: 2021/02/22 14:58:59 by nbarreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*ptr;
-	size_t	s_len;
+	char	*str;
+	size_t	size;
 
 	if (!s)
 		return (0);
-	s_len = ft_strlen(s) + 1;
-	if (start > s_len)
+	size = ft_strlen(s) + 1;
+	if (start > size)
 		return (ft_strdup(""));
-	ptr = (char *)malloc(sizeof(char) * (len + 1));
-	if (!ptr)
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
 		return (0);
-	ft_strlcpy(ptr, (s + start), len + 1);
-	ptr[len] = 0;
-	return (ptr);
+	ft_strlcpy(str, (s + start), len + 1);
+	str[len] = 0;
+	return (str);
 }
